87b379e80d8e96afff1d1fb776c584096f4b1927
Brandon Coffey <bmc180001@utdallas.edu>
2021-10-27 11:01:21 -0500

    General Utility Functions

---
+++ Assets/_Game/Scripts/Utility/Other/DebugDisabler.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+namespace Scripts.Utility.Other
+{
+    public class DebugDisabler : MonoBehaviour
+    {
+        [SerializeField] private bool _debugMode = true;
+        private bool _unityDebugMode = true;
+
+        public bool DebugMode
+        {
+            get => _debugMode;
+            set
+            {
+                SetDebugMode(value);
+                _debugMode = value;
+            }
+        }
+
+        private void Awake()
+        {
+            SetDebugMode(_debugMode);
+        }
+
+        private void OnValidate()
+        {
+            SetDebugMode(_debugMode);
+        }
+
+
+        private void SetDebugMode(bool mode)
+        {
+#if UNITY_EDITOR
+            if (mode != _unityDebugMode) {
+                Debug.unityLogger.logEnabled = mode;
+                _unityDebugMode = mode;
+            }
+#else
+            Debug.unityLogger.logEnabled = false;
+#endif
+        }
+    }
+}
